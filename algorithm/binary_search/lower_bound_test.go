b08e4481fbad2e57b1855436fad30f2711eebd4b
jiang4869 <1121429190@qq.com>
2022-04-11 00:15:53 +0800

    更新lower_bound测试用例

--- algorithm/binary_search/lower_bound_test.go
+++ algorithm/binary_search/lower_bound_test.go
@@ -8,6 +8,6 @@ import (
 func Test_Case1(t *testing.T) {
 	nums := []int{1, 2, 3, 4, 4, 6}
 	key := 7
-	idx := LowerBound(nums, key)
+	idx := LowerBound(nums, 0, len(nums), key)
 	fmt.Println(idx)
 }
